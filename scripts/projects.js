b7b3fe731dbebf3fdb0abbd3831dc95db6ecfa2f
Hechen Liu <codingchallenge4@gmail.com>
2022-07-07 10:59:44 -0400

    add growing fruit

--- scripts/projects.js
+++ scripts/projects.js
@@ -2,12 +2,75 @@ let numProjects = 5;
 gsap.to(".parallex-bg", {
   scrollTrigger: {
     scrub: true,
-    snap: 1/(numProjects-1)
+    snap: 1/4,
   },
   // y: (index, target) => -ScrollTrigger.maxScroll(window) * target.dataset.speed,
-  y: (index, target) => {
-    // console.log(-ScrollTrigger.maxScroll(window));
-    return -ScrollTrigger.maxScroll(window) * target.dataset.speed;
-  },
+  y: (index, target) => -ScrollTrigger.maxScroll(window) * target.dataset.speed,
+  // onSnapComplete: function(self) {
+  //   console.log(numProjects);
+  //   // let address = "./media/branch/branch-" + numProjects;
+  //   // gsap.to(".background", {
+  //   //   "background-image": ("url(" + address + ")"),
+  //   //   duration: 1
+  //   // });
+  //   numProjects--;
+  // },
   ease: "none", 
 });
+
+// gsap.to(".background", {
+//   scrollTrigger: {
+//     trigger: "#bg-2-1",
+//     start: "top top",
+//     end: "+=100vh"
+//   },
+//   "background-image": ("url(" + "./media/branch/branch-5.png)"),
+//   ease: "none", 
+//   duration: 0
+// });
+
+gsap.to(".background", {
+  scrollTrigger: {
+    trigger: "#bg-2-4",
+    start: "bottom center",
+    end: "+=100vh"
+  },
+  "background-image": "url(../media/branch/branch-4.png)",
+  ease: "power", 
+  duration: 0.1
+});
+
+gsap.to(".background", {
+  scrollTrigger: {
+    trigger: "#bg-2-7",
+    start: "bottom center",
+    end: "+=100vh"
+  },
+  "background-image": "url(../media/branch/branch-3.png)",
+  ease: "none", 
+  duration: 0.1
+});
+
+gsap.to(".background", {
+  scrollTrigger: {
+    trigger: "#bg-2-10",
+    start: "bottom center",
+    end: "+=100vh"
+  },
+  "background-image": "url(../media/branch/branch-2.png)",
+  ease: "none", 
+  duration: 0.1
+});
+
+gsap.to(".background", {
+  scrollTrigger: {
+    trigger: "#bg-2-13",
+    start: "bottom center",
+    end: "+=100vh"
+  },
+  "background-image": "url(../media/branch/branch-1.png)",
+  ease: "none", 
+  duration: 0.1
+});
+
+
